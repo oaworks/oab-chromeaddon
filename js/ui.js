1d1c6e0e8e6dbca6cf62483e750559730b453a4f
Andy Byers <ajrbyers@andys-mbp.gateway.2wire.net>
2014-10-09 15:53:36 +0100

    Login, Register and Logout are all working now.

--- js/ui.js
+++ js/ui.js
@@ -1,49 +1,70 @@
 var current_page = location.pathname
 var key = localStorage.getItem("api_key");
 
+function login_register_api_call(api_request, data) {
+	var response = ''
+	var serviceaddress = 'http://oabutton.cottagelabs.com';
+	var apiaddress = serviceaddress + '/api';
+	var api_key = '';
+	var username = '';
+    $.ajax({
+        'type': 'POST',
+        'url': apiaddress + api_request,
+        'contentType': 'application/json; charset=utf-8',
+        'dataType': 'JSON',
+        'processData': false,
+        'cache': false,
+        'data': data,
+        'success': function(data){
+        	console.log(data)
+    		localStorage.setItem("api_key", data.api_key);
+    		window.location.href = "login.html"
+    	},
+        'error': function(data){
+        	console.log(data)
+    	},
+    });
+}
+
 if (current_page == '/ui/login.html') {
 	window.addEventListener('load', function () {
 
-	if (key){
-		window.location.href = '/ui/main.html';
-	}
+		// If we have a key, redirect to the main page.
+		if (key){
+			window.location.href = '/ui/main.html';
+		}
 
-    document.getElementById("signup").addEventListener('click', function(){
-    	var response = ''
-    	var serviceaddress = 'http://oabutton.cottagelabs.com';
-		var apiaddress = serviceaddress + '/api';
-		var api_key = '';
-		var username = '';
-	    $.ajax({
-	        'type': 'POST',
-	        'url': apiaddress + '/register',
-	        'contentType': 'application/json; charset=utf-8',
-	        'dataType': 'JSON',
-	        'processData': false,
-	        'cache': false,
-	        'data': JSON.stringify({
+		// Handle the register button.
+	    document.getElementById("signup").addEventListener('click', function(){
+	    	var api_request = '/register';
+	    	data = JSON.stringify({
 	            'email': document.getElementById("user_email").value,
 	            'password': document.getElementById('user_password').value,
-	        }),
-	        'success': function(data){
-	        	console.log(data)
-        		localStorage.setItem("api_key", data.api_key);
-        	},
-	        'error': function(data){
-            	console.log(data)
-        	},
+	        });
+	    	login_register_api_call(api_request, data);
 	    });
 
-    });
-
-    // Handle login
-    document.getElementById("login").addEventListener('click', function(){
-    	window.location.href = "main.html";	
-    });
+	    // Handle the login button.
+	    document.getElementById("login").addEventListener('click', function(){
+	    	var api_request = '/retrieve';
+	    	data = JSON.stringify({
+	            'email': document.getElementById("user_email").value,
+	            'password': document.getElementById('user_password').value,
+	        });
+	    	login_register_api_call(api_request, data);
+	    });
 
 	});
+
 } else if (current_page == '/ui/main.html' && key) {
-	console.log(key)
+	window.addEventListener('load', function () {
+		// Handle the logout button.
+		document.getElementById("logout").addEventListener('click', function(){
+    		if ('api_key' in localStorage) localStorage.removeItem('api_key');
+    		window.location.href = 'login.html';
+    	});
+	});
+
 } else {
 	window.location.href = "login.html";	
 }
