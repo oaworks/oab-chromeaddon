f1f8d19b5d32d354240bd2f1bfc0bc470a522adb
Andy Byers <andy.byers@ubiquitypress.com>
2014-10-10 14:28:47 +0100

    Added error catch for missing credentials, added some helpers. update mining.

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ Open Access Button Chrome Extension
 To Do
 -----
 
-- ~~Register~~
-- ~~Login~~
+- ~~Register.~~
+- ~~Login.~~
+- ~~Catch if there is no email or password.~~
+- ~~Write display function for errors (bootstrap notices).~~
 - Write generic API caller with handler functions for sucess.
-- Catch if there is no email or password.
-- Write display function for errors (bootstrap notices.)
+- Add block event on load.
+- Tidy up and refactor code into functions.
+
--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,18 @@
 var current_page = location.pathname
-var key = localStorage.getItem("api_key");
+var key = localStorage.getItem('api_key');
 var serviceaddress = 'http://oabutton.cottagelabs.com';
 var apiaddress = serviceaddress + '/api';
 
 
 // Helpers
 function get_id(id) { return document.getElementById(id); }
+function get_value(id) { return document.getElementById(id).value; }
+
+function display_error(id, warning){
+	var warn_div = get_id('error');
+	var content = '<div class="alert alert-danger" role="alert">' + warning + '</div>';
+	warn_div.innerHTML = content;
+}
 
 function get_active_tab() {
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
@@ -15,8 +22,14 @@ function get_active_tab() {
 }
 
 function handle_data(data) {
-	document.getElementById("api_content").innerHTML = "<h5>" + data.contentmine.metadata['title'] + '</h5>';
-	document.getElementById("api_content").innerHTML += "<ul><li>Blocked: " + data.blocked + '</li>' + "<li>Wishlist: " + data.wishlist + '</li></ul>';
+	var api_div = get_id('api_content');
+	console.log(data);
+	if (data.contentmine.metadata['title']){
+		api_div.innerHTML = '<h5>' + data.contentmine.metadata['title'] + '</h5>';
+	} else {
+		api_div.innerHTML = '<h5>About this article</h5><ul><li>Blocked: ' + data.blocked + '</li>' + '<li>Wishlist: ' + data.wishlist + '</li></ul>';
+	}
+	
 }
 
 function login_register_api_call(api_request, data) {
@@ -32,8 +45,8 @@ function login_register_api_call(api_request, data) {
         'cache': false,
         'data': data,
         'success': function(data){
-    		localStorage.setItem("api_key", data.api_key);
-    		window.location.href = "login.html"
+    		localStorage.setItem('api_key', data.api_key);
+    		window.location.href = 'login.html'
     	},
         'error': function(data){
         	// Todo: Handle error here.
@@ -75,23 +88,39 @@ if (current_page == '/ui/login.html') {
 		}
 
 		// Handle the register button.
-	    document.getElementById("signup").addEventListener('click', function(){
-	    	var api_request = '/register';
-	    	data = JSON.stringify({
-	            'email': document.getElementById("user_email").value,
-	            'password': document.getElementById('user_password').value,
-	        });
-	    	login_register_api_call(api_request, data);
+	    document.getElementById('signup').addEventListener('click', function(){
+	    	var user_email = get_value('user_email');
+	    	var user_password = get_value('user_password');
+
+	    	if (user_email && user_password){
+	    		var api_request = '/register';
+		    	data = JSON.stringify({
+		            'email': user_email,
+		            'password': user_password,
+		        });
+		    	login_register_api_call(api_request, data);
+	    	} else {
+	    		display_error('error', 'You must supply an email address and a password to login or register.');
+	    	}
+
+	    	
 	    });
 
 	    // Handle the login button.
-	    document.getElementById("login").addEventListener('click', function(){
-	    	var api_request = '/retrieve';
-	    	data = JSON.stringify({
-	            'email': document.getElementById("user_email").value,
-	            'password': document.getElementById('user_password').value,
-	        });
-	    	login_register_api_call(api_request, data);
+	    document.getElementById('login').addEventListener('click', function(){
+	    	var user_email = get_value('user_email');
+	    	var user_password = get_value('user_password');
+
+	    	if (user_email && user_password){
+	    		var api_request = '/retrieve';
+		    	data = JSON.stringify({
+		            'email': user_email,
+		            'password': user_password,
+		        });
+	    		login_register_api_call(api_request, data);
+	    	} else {
+	    		display_error('error', 'You must supply an email address and a password to login or register.');
+	    	}
 	    });
 
 	});
@@ -99,13 +128,13 @@ if (current_page == '/ui/login.html') {
 } else if (current_page == '/ui/main.html' && key) {
 	window.addEventListener('load', function () {
 		// Handle the logout button.
-		document.getElementById("logout").addEventListener('click', function(){
+		document.getElementById('logout').addEventListener('click', function(){
     		if ('api_key' in localStorage) localStorage.removeItem('api_key');
     		window.location.href = 'login.html';
     	});
-		status_meta = status_request(localStorage.getItem("active_tab"), '/status');
+		status_meta = status_request(localStorage.getItem('active_tab'), '/status');
 	});
 
 } else {
-	window.location.href = "login.html";	
+	window.location.href = 'login.html';	
 }
